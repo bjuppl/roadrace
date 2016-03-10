7858b5644a86e068307278c25df88d1b6685a41b
Zach <zpalm466@students.bju.edu>
2016-03-10 09:32:31 -0500

    30 more  minutes of work

--- road_race/highscore.cpp
+++ road_race/highscore.cpp
@@ -2,3 +2,14 @@
 #include <roadrace.h>
 #include <iostream>
 
+bool HighScore::load()
+{
+    //ifstream ifile("highscore.txt");
+    //if (ifile) {
+      //retrieve contents
+    //} else {
+      //create highscore.txt
+    //}
+
+
+}
--- road_race/highscore.h
+++ road_race/highscore.h
@@ -6,15 +6,30 @@
 using namespace std;
 
 
-class highscore
+class HighScore
 {
-    private
+private:
 
+    //vector to store the high score list during each run of the game
     vector<string> highScoreList;
 
-    public
+public:
 
+    //Checks to see if a high score file exists and creates it if it does not already exist
+    //returns true if a file was already found and false if it had to create one.`
+    bool load();
 
+    //Adds a score to the loaded vector using a name, score and some info
+    void addScore(string name, int score, string info);
+
+    //Clears the current high score list
+    void clearScore();
+
+    //Prints out the high score list in a nicely formatted manner
+    void print();
+
+    //Update the highscore file with the updated information from the last game run
+    void update();
 
 };
 
