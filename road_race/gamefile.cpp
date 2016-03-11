5c83100e1269095a7ea27158a5a08441697259af
jbraz767 <jbraz767@students.bju.edu>
2016-03-11 14:24:28 -0500

    Added GameFileManager class

--- road_race/gamefile.cpp
+++ road_race/gamefile.cpp
@@ -1,2 +1,11 @@
 #include "gamefile.h"
 
+GameFileManager::GameFileManager (std::string contents ) {
+
+
+}
+
+GameFileManager GameFileManager::fromFile( std::string fileName ) {
+
+    return GameFileManager("test");
+}
--- road_race/gamefile.h
+++ road_race/gamefile.h
@@ -1,4 +1,16 @@
 #ifndef GAMEFILE_H
 #define GAMEFILE_H
 
+
+#include <string>
+
+class GameFileManager {
+
+public:
+    GameFileManager(std::string contents );
+    static GameFileManager fromFile( std::string fileName );
+
+};
+
+
 #endif // GAMEFILE_H
