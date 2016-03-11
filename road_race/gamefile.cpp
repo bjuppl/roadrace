905e6e49fd5446234740d57d26b8e8ad69defb4b
jbraz767 <jbraz767@students.bju.edu>
2016-03-11 14:35:40 -0500

    Fleshed out GameFileManager::fromFile factory function

--- road_race/gamefile.cpp
+++ road_race/gamefile.cpp
@@ -1,11 +1,28 @@
 #include "gamefile.h"
 
-GameFileManager::GameFileManager (std::string contents ) {
+GameFileManager::GameFileManager (std::vector<std::string> contents ) {
 
 
 }
 
 GameFileManager GameFileManager::fromFile( std::string fileName ) {
 
-    return GameFileManager("test");
+    std::string line;
+
+    std::vector<std::string> contents;
+    std::ifstream gamefile (fileName);
+    if (gamefile.is_open())
+    {
+      while ( std::getline (gamefile,line) )
+      {
+        contents.push_back(line);
+      }
+      gamefile.close();
+    }
+
+    else {
+        std::cerr << "Game file " << fileName << " does not exist.";
+    }
+
+    return GameFileManager(contents);
 }
