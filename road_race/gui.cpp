5bd7dc37db1893491d2c60395a0ef67668e3f864
jprid151 <jprid151@students.bju.edu>
2016-03-14 22:03:29 -0400

    click

--- road_race/gui.cpp
+++ road_race/gui.cpp
@@ -1,6 +1,8 @@
 #include "gui.h"
 #include "roadrace.h"
 #include "gamemodel.h"
+#include "ui_roadrace.h"
+#include <QLabel>
 GuiManager *GuiManager::instance_ = NULL;
 
 GuiManager& GuiManager::instance() {
@@ -14,8 +16,7 @@ GuiManager& GuiManager::instance() {
 void GuiManager::generateSquareGrid() {
     std::cout << getUi()->label->text().toStdString() << std::endl;
 
-    int hi = Game::instance().getHeight();
-    int wi = Game::instance().getWidth();
+
     vector<vector<Square*>> squaresList = Game::instance().getSquares();
     int i2 = 0;
      int hit=1;
@@ -32,11 +33,12 @@ void GuiManager::generateSquareGrid() {
        SquareLabel *lbl = new SquareLabel(proc,ui->gridLayoutWidget);
        QPixmap map = setmap(proc);
        lbl->setPixmap(map);
-        lbl->resize(40,40);
-       ui->gameLayout->addWidget(lbl,wid,hit,wid+1,hit,0);
+        lbl->setFixedHeight(40);
+        lbl->setFixedWidth(40);
+       ui->gameLayout->addWidget(lbl,wid,hit,0);
 
-       int w = lbl->width();
-       int h = lbl->height();
+      proc->setX(wid);
+      proc->setY(hit);
        lbl->show();
     }
     i2++;
--- road_race/square.cpp
+++ road_race/square.cpp
@@ -1,1 +1,40 @@
 #include "square.h"
+#include "gamemodel.h"
+#include <QMouseEvent>
+#include <QWidget>
+#include <QLabel>
+void SquareLabel::mousePressEvent(QMouseEvent *ev){
+
+    vector<vector<Square*>> squareList = Game::instance().getSquares();
+
+    Square *proc = square;
+    int x = this->x();
+    if (proc->getOwner() == nullptr){
+        int x = proc->getX();
+        int y = proc->getY();
+        int height = Game::instance().getHeight();
+        int width = Game::instance().getWidth();
+        QString owner;
+        /*if((x-1) >0){
+            Square *comp = Game::instance().getSquare(x+1,y);
+            Player *newowner = comp->getOwner();
+            proc->setOwner(newowner);
+            owner = QString::fromUtf8(newowner->getName().c_str());
+        }
+        if((x+1) <= width){
+            Square *comp = Game::instance().getSquare(x+1,y);
+        }
+        if((y-1) > 0){
+            Square *comp = Game::instance().getSquare(x+1,y);
+        }
+        if((y+1) <= height){
+            Square *comp = Game::instance().getSquare(x+1,y);
+        */}
+
+    else{
+            QString owner;
+            owner = QString::fromUtf8(proc->getOwner()->getName().c_str());
+            this->setText(owner);
+    }
+    emit clicked();
+}
