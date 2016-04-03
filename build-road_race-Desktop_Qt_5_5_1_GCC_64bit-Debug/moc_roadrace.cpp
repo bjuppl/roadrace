bf87e3e7801a95bf1d54d204723a4b892e906ab7
Zach <zpalm466@students.bju.edu>
2016-04-03 16:51:17 -0400

    all the sound and charred image added

--- build-road_race-Desktop_Qt_5_5_1_GCC_64bit-Debug/moc_roadrace.cpp
+++ build-road_race-Desktop_Qt_5_5_1_GCC_64bit-Debug/moc_roadrace.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_RoadRace_t {
-    QByteArrayData data[13];
-    char stringdata0[194];
+    QByteArrayData data[18];
+    char stringdata0[251];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,22 +34,29 @@ QT_MOC_LITERAL(1, 9, 19), // "on_btnStuff_clicked"
 QT_MOC_LITERAL(2, 29, 0), // ""
 QT_MOC_LITERAL(3, 30, 12), // "dataReceived"
 QT_MOC_LITERAL(4, 43, 18), // "serverDisconnected"
-QT_MOC_LITERAL(5, 62, 8), // "loadFile"
-QT_MOC_LITERAL(6, 71, 22), // "on_btnOpenGame_clicked"
-QT_MOC_LITERAL(7, 94, 12), // "labelClicked"
-QT_MOC_LITERAL(8, 107, 18), // "on_helpBtn_clicked"
-QT_MOC_LITERAL(9, 126, 19), // "on_cheatBtn_clicked"
-QT_MOC_LITERAL(10, 146, 9), // "openCheat"
-QT_MOC_LITERAL(11, 156, 18), // "on_SaveBtn_clicked"
-QT_MOC_LITERAL(12, 175, 18) // "on_tst2Btn_clicked"
+QT_MOC_LITERAL(5, 62, 14), // "connect_server"
+QT_MOC_LITERAL(6, 77, 4), // "send"
+QT_MOC_LITERAL(7, 82, 8), // "loadFile"
+QT_MOC_LITERAL(8, 91, 22), // "on_btnOpenGame_clicked"
+QT_MOC_LITERAL(9, 114, 12), // "labelClicked"
+QT_MOC_LITERAL(10, 127, 18), // "on_helpBtn_clicked"
+QT_MOC_LITERAL(11, 146, 19), // "on_cheatBtn_clicked"
+QT_MOC_LITERAL(12, 166, 9), // "openCheat"
+QT_MOC_LITERAL(13, 176, 18), // "on_SaveBtn_clicked"
+QT_MOC_LITERAL(14, 195, 18), // "on_tst2Btn_clicked"
+QT_MOC_LITERAL(15, 214, 17), // "on_RanBtn_clicked"
+QT_MOC_LITERAL(16, 232, 9), // "openMulti"
+QT_MOC_LITERAL(17, 242, 8) // "openJoin"
 
     },
     "RoadRace\0on_btnStuff_clicked\0\0"
     "dataReceived\0serverDisconnected\0"
-    "loadFile\0on_btnOpenGame_clicked\0"
-    "labelClicked\0on_helpBtn_clicked\0"
-    "on_cheatBtn_clicked\0openCheat\0"
-    "on_SaveBtn_clicked\0on_tst2Btn_clicked"
+    "connect_server\0send\0loadFile\0"
+    "on_btnOpenGame_clicked\0labelClicked\0"
+    "on_helpBtn_clicked\0on_cheatBtn_clicked\0"
+    "openCheat\0on_SaveBtn_clicked\0"
+    "on_tst2Btn_clicked\0on_RanBtn_clicked\0"
+    "openMulti\0openJoin"
 };
 #undef QT_MOC_LITERAL
 
@@ -59,7 +66,7 @@ static const uint qt_meta_data_RoadRace[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      11,   14, // methods
+      16,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -67,19 +74,29 @@ static const uint qt_meta_data_RoadRace[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   69,    2, 0x08 /* Private */,
-       3,    0,   70,    2, 0x08 /* Private */,
-       4,    0,   71,    2, 0x08 /* Private */,
-       5,    0,   72,    2, 0x08 /* Private */,
-       6,    0,   73,    2, 0x08 /* Private */,
-       7,    0,   74,    2, 0x08 /* Private */,
-       8,    0,   75,    2, 0x08 /* Private */,
-       9,    0,   76,    2, 0x08 /* Private */,
-      10,    0,   77,    2, 0x08 /* Private */,
-      11,    0,   78,    2, 0x08 /* Private */,
-      12,    0,   79,    2, 0x08 /* Private */,
+       1,    0,   94,    2, 0x08 /* Private */,
+       3,    0,   95,    2, 0x08 /* Private */,
+       4,    0,   96,    2, 0x08 /* Private */,
+       5,    0,   97,    2, 0x08 /* Private */,
+       6,    0,   98,    2, 0x08 /* Private */,
+       7,    0,   99,    2, 0x08 /* Private */,
+       8,    0,  100,    2, 0x08 /* Private */,
+       9,    0,  101,    2, 0x08 /* Private */,
+      10,    0,  102,    2, 0x08 /* Private */,
+      11,    0,  103,    2, 0x08 /* Private */,
+      12,    0,  104,    2, 0x08 /* Private */,
+      13,    0,  105,    2, 0x08 /* Private */,
+      14,    0,  106,    2, 0x08 /* Private */,
+      15,    0,  107,    2, 0x08 /* Private */,
+      16,    0,  108,    2, 0x08 /* Private */,
+      17,    0,  109,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -104,14 +121,19 @@ void RoadRace::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, vo
         case 0: _t->on_btnStuff_clicked(); break;
         case 1: _t->dataReceived(); break;
         case 2: _t->serverDisconnected(); break;
-        case 3: _t->loadFile(); break;
-        case 4: _t->on_btnOpenGame_clicked(); break;
-        case 5: _t->labelClicked(); break;
-        case 6: _t->on_helpBtn_clicked(); break;
-        case 7: _t->on_cheatBtn_clicked(); break;
-        case 8: _t->openCheat(); break;
-        case 9: _t->on_SaveBtn_clicked(); break;
-        case 10: _t->on_tst2Btn_clicked(); break;
+        case 3: _t->connect_server(); break;
+        case 4: _t->send(); break;
+        case 5: _t->loadFile(); break;
+        case 6: _t->on_btnOpenGame_clicked(); break;
+        case 7: _t->labelClicked(); break;
+        case 8: _t->on_helpBtn_clicked(); break;
+        case 9: _t->on_cheatBtn_clicked(); break;
+        case 10: _t->openCheat(); break;
+        case 11: _t->on_SaveBtn_clicked(); break;
+        case 12: _t->on_tst2Btn_clicked(); break;
+        case 13: _t->on_RanBtn_clicked(); break;
+        case 14: _t->openMulti(); break;
+        case 15: _t->openJoin(); break;
         default: ;
         }
     }
@@ -143,13 +165,13 @@ int RoadRace::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 11)
+        if (_id < 16)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 11;
+        _id -= 16;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 11)
+        if (_id < 16)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 11;
+        _id -= 16;
     }
     return _id;
 }
