ecf4cb696f04e9685d25926e716c97877f2f99df
jprid151 <jprid151@students.bju.edu>
2016-04-08 18:00:40 -0400

    Bugfix for square

--- road_race/player.h
+++ road_race/player.h
@@ -42,6 +42,11 @@ public:
        void setScore(int sc){ score = sc;}
        void setStartTime(int st){ startTime = st;}
        void setSwamped(bool sw){swamped = sw;}
+       
+       
+       
+       
+       
        ~Player(){
 
        }
